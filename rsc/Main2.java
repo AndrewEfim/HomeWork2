4223449b6386666556b8ae7f93220cb2f625516e
Andreii <n.say.d@mail.ru>
2018-05-26 14:23:17 +0300

    Add files

---
+++ rsc/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int a = 4537;
+        int b = a%10;
+        if (b==7)
+            System.out.println("Последняя цифра "  + b);
+        else 
+            System.out.println("Последняя цифра не 7"); 
+    }
+}
